2b44573ba7479e750e4ca4ae51fcc749dcf36483
Jordan Diaz <JordanDiaz@Jordans-MacBook-Pro.local>
2019-12-12 23:43:28 -0500

    header complete, some js added, item list in progress

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+var button = document.getElementById("add-item");
+
+//button click
+//when button is clicked pull input value and create to-do list item
+function processClick() {
+    var value = document.getElementById("item").value;
+    
+    if (value) {
+        
+    }
+}
+
+//event listener
+button.addEventListener("click", processClick);
