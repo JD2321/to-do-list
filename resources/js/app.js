5968116383ef82867967b54e8955f3b6a4d8e7f8
Jordan Diaz <JordanDiaz@Jordans-MacBook-Pro.local>
2019-12-22 20:02:49 -0500

    edits made to JS. duplicate entries fixed. stil working on check button functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,36 @@
 //THINGS TO COMPLETE
 //add remove button functionality 
-//add check button functionality
-//add done check button functionality
-//fix repeat items after each add
 
+//add done check button functionality
+
+//****** = items affecting current task
 
 
 
 
 var button = document.getElementById("add-item");
+var doneButton = document.querySelector("#check");
 
 //button icons
 var checkIcon = '<i id="check" class="fas fa-check-circle fa-2x"></i>';
 var removeIcon = '<i id="remove" class="far fa-trash-alt fa-2x"></i>';
 
 //arrays
-var todoItems = []
-var doneItems = []
+var toDoListArr = ["test"]
+var doneItemsArr = []
 
 //button click
 //when button is clicked pull input value and create to-do list item
 function processClick() {
     var value = document.getElementById("item").value;
     var input = document.getElementById("item");
+    var toDoList = document.getElementById("in-prog");
+    var doneList = document.getElementById("done")
     
     if (value) {
+        toDoList.innerHTML = ""
         addItem(value);
-        todoItems.forEach(createItem);
+        toDoListArr.forEach(createItem);
     }
 
  input.value = "";
@@ -35,9 +39,20 @@ function processClick() {
 //add item to array
 function addItem(text) {
     var newItem = text;
-    todoItems.unshift(newItem);
-    console.log(todoItems);
+    toDoListArr.unshift(newItem);
+    console.log(toDoListArr);
 }
+
+//****
+function verifyCheckButton(doneButton) {
+    if (document.body.contains(doneButton)) {
+        doneButton.addEventListener("click", processCheck);
+        console.log("click button!")
+    } else {
+        console.log("no click button!")
+    };
+}
+
 
 // create new list item 
 function createItem(text) {
@@ -66,5 +81,24 @@ function createItem(text) {
 
 }
 
+//******
+function searchStringInArray (str, strArray) {
+    for (var j=0; j<strArray.length; j++) {
+        if (strArray[j].match(str)) return j;
+    }
+    return -1;
+    
+}
+
+//****** 
+//when check button is clicked item moves to done list
+function processCheck() {
+ console.log("clicked!")
+}
+
 //event listener
 button.addEventListener("click", processClick);
+verifyCheckButton();
+
+
+console.log(searchStringInArray("test", toDoListArr));
